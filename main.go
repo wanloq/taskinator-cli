5682d99d50c9f5143871f4dfa23e0ea7c2957ae5
wanloq <mcjinkz@gmail.com>
2025-02-22 11:26:51 +0100

    New update

--- main.go
+++ main.go
@@ -2,6 +2,39 @@ package main
 
 import "fmt"
 
+type Task struct {
+	ID          int
+	Description string
+	Status      bool
+}
+
+var tasks []Task
+
 func main() {
-	fmt.Println("Hello Person!")
+	addTask("Template one")
+	addTask("Template two")
+	addTask("Template three")
+	viewTasks()
+}
+
+func addTask(description string) {
+	id := len(tasks) + 1
+	task := Task{
+		ID:          id,
+		Description: description,
+		Status:      false,
+	}
+	tasks = append(tasks, task)
+	fmt.Println("Task added successfully")
+}
+
+func viewTasks() {
+	fmt.Print("ID\t|Status\t|\tDescription\n")
+	for _, task := range tasks {
+		if len(tasks) != 0 {
+			fmt.Printf("%v\t|%v\t|%v\n", task.ID, task.Status, task.Description)
+		} else {
+			fmt.Println("No tasks for now")
+		}
+	}
 }
