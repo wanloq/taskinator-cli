1708a0de33bb9ca2e0d813cdbc6e2adf8fe11627
wanloq <mcjinkz@gmail.com>
2025-02-23 09:45:38 +0100

    New update

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ As my first mini-project on this journey, I’ll build a simple CLI-based task m
 ✅ List all tasks (showing task number, description, and completion status) 
 ✅ Mark tasks as complete 
 ✅ Delete tasks.
+✅ List completed tasks.
+✅ List incompleted tasks.
+✅ Use JSON encoding/decoding.
+✅ Save tasks to file.
+✅ Load tasks from file.
--- main.go
+++ main.go
@@ -1,25 +1,105 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 type Task struct {
-	ID          int
-	Description string
-	Status      bool
+	ID          int    `json:"id"`
+	Description string `json:"description"`
+	Status      bool   `json:"status"`
 }
 
 var tasks []Task
 
 func main() {
-	addTask("Template one")
-	addTask("Template two")
-	addTask("Template three")
-	taskComplete(3)
-	viewTasks()
+	/*
+	 ADD-Go to church on sunday
+	 ADD-Read a book
+	 ADD-Read the bible
+	 ADD-Drink water
+	 ADD-Go to the Gym at 15:00
+	*/
+	loadTasks()
+	commands := map[string]string{
+		"Add new task":           "ADD-\"Task name\"",
+		"Delete a task":          "DELETE-\"Task ID\"",
+		"View all tasks":         "VIEW ALL",
+		"View Completed tasks":   "VIEW COMPLETED",
+		"View Pending tasks":     "VIEW PENDING",
+		"Mark task as Completed": "MARK COMPLETE-\"Task ID\"",
+	}
+
+	for {
+		fmt.Println("\n\tCLI TASKINATOR IS RUNNING")
+		fmt.Print("\nTo perform a command, Please enter the prompt below without the quotation marks (e.g. ADD-Read a book) \n\n")
+		for command, prompt := range commands {
+			fmt.Printf("To %v:: \t%v\n", command, prompt)
+		}
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("\nEnter a command: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		inputSlice := strings.Split(input, "-")
+		command := strings.ToUpper(inputSlice[0])
+		var arg string
+		if len(inputSlice) <= 1 {
+			if command != "ADD" && command != "SAVE" && command != "DELETE" && command != "VIEW ALL" && command != "VIEW COMPLETED" && command != "VIEW PENDING" && command != "MARK COMPLETE" {
+				fmt.Println("Invalid input, Try again")
+				continue
+			}
+		}
+		if len(inputSlice) >= 2 {
+			arg = inputSlice[1]
+		}
+
+		switch command {
+		case "ADD":
+			addTask(arg)
+		case "DELETE":
+			argInt, _ := strconv.Atoi(strings.TrimSpace(arg))
+			deleteTask(argInt)
+		case "VIEW ALL":
+			viewTasks()
+		case "VIEW COMPLETED":
+			viewCompletedTasks()
+		case "VIEW PENDING":
+			viewIncompletedTasks()
+		case "MARK COMPLETE":
+			argInt, _ := strconv.Atoi(strings.TrimSpace(arg))
+			taskComplete(argInt)
+		case "SAVE":
+			save()
+		default:
+			fmt.Println("Command not supported")
+		}
+	}
+}
+
+func loadTasks() {
+	jsonData, _ := os.ReadFile("tasks.json")
+	json.Unmarshal(jsonData, &tasks)
+	fmt.Println("\nSaved tasks successfully loaded to memeory")
+}
+
+func save() {
+	jsonData, _ := json.MarshalIndent(tasks, "", " ")
+	os.WriteFile("tasks.json", jsonData, 0644)
+	fmt.Println("Changes Saved Successfully!")
 }
 
 func addTask(description string) {
 	id := len(tasks) + 1
+	for _, task := range tasks {
+		if task.ID >= id {
+			id = task.ID + 1
+		}
+	}
 	task := Task{
 		ID:          id,
 		Description: description,
@@ -27,6 +107,69 @@ func addTask(description string) {
 	}
 	tasks = append(tasks, task)
 	fmt.Println("Task added successfully")
+	save()
+}
+
+func viewTasks() {
+	fmt.Println("\nViewing all tasks")
+	fmt.Print("ID\t|Status\t|\tDescription\n")
+	if len(tasks) > 0 {
+		for _, task := range tasks {
+			status := "❌"
+			if task.Status {
+				status = "✅"
+			}
+			fmt.Printf("%v\t|%v\t|%v\n", task.ID, status, task.Description)
+		}
+	} else {
+		fmt.Println("\tNo tasks for now")
+	}
+}
+
+func viewCompletedTasks() {
+	fmt.Println("\nViewing completed tasks")
+	fmt.Print("ID\t|Status\t|\tDescription\n")
+	if len(tasks) > 0 {
+		found := false
+		for _, task := range tasks {
+			if task.Status == true {
+				found = true
+				status := "❌"
+				if task.Status {
+					status = "✅"
+				}
+				fmt.Printf("%v\t|%v\t|%v\n", task.ID, status, task.Description)
+			}
+		}
+		if !found {
+			fmt.Println("\tNo completed tasks yet")
+		}
+	} else {
+		fmt.Println("\tNo tasks for now")
+	}
+}
+
+func viewIncompletedTasks() {
+	fmt.Println("\nViewing incompleted tasks")
+	fmt.Print("ID\t|Status\t|\tDescription\n")
+	if len(tasks) > 0 {
+		found := false
+		for _, task := range tasks {
+			if task.Status == false {
+				found = true
+				status := "❌"
+				if task.Status {
+					status = "✅"
+				}
+				fmt.Printf("%v\t|%v\t|%v\n", task.ID, status, task.Description)
+			}
+		}
+		if !found {
+			fmt.Println("\tNo completed tasks yet")
+		}
+	} else {
+		fmt.Println("\tNo tasks for now")
+	}
 }
 
 func taskComplete(id int) {
@@ -37,7 +180,7 @@ func taskComplete(id int) {
 		for i, task := range tasks {
 			if task.ID == id {
 				tasks[i].Status = true
-				fmt.Printf("Task %v Marked completed\n", task.ID)
+				fmt.Printf("%v Marked completed\n", task.Description)
 				found = true
 				break
 			}
@@ -46,16 +189,26 @@ func taskComplete(id int) {
 			fmt.Println("\tTask not found")
 		}
 	}
+	save()
 }
 
-func viewTasks() {
-	fmt.Print("ID\t|Status\t|\tDescription\n")
-	if len(tasks) > 0 {
-		for _, task := range tasks {
-			fmt.Printf("%v\t|%v\t|%v\n", task.ID, task.Status, task.Description)
-		}
+func deleteTask(id int) {
+	found := false
+	if len(tasks) == 0 {
+		fmt.Printf("\tTask with ID:%v not found\n", id)
 	} else {
-		fmt.Println("\tNo tasks for now")
+		for i, task := range tasks {
+			if task.ID == id {
+				tasks = append(tasks[:i], tasks[i+1:]...)
+				fmt.Printf("Task %v Successfully Deleted\n", id)
+				found = true
+				break
+			}
+		}
+		if !found {
+			fmt.Println("\tTask not found")
+		}
 	}
+	save()
 
 }
