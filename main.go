56a95059440e9bd524478b4d43a545a69dfa6d0b
wanloq <mcjinkz@gmail.com>
2025-03-09 18:07:44 +0100

    New update

--- main.go
+++ main.go
@@ -7,27 +7,29 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"slices"
 	"strconv"
 	"strings"
 	"sync"
 	"syscall"
+	"time"
 )
 
 type Task struct {
-	ID          int    `json:"id"`
-	Description string `json:"description"`
-	Status      bool   `json:"status"`
-	Priority    int    `json:"priority"`
-	DueDate     string `json:"dueDate"`
+	ID          int       `json:"id"`
+	Description string    `json:"description"`
+	Status      bool      `json:"status"`
+	Priority    int       `json:"priority"`
+	DueDate     time.Time `json:"dueDate"`
 }
 
 type Taskinator interface {
-	AddTask(description string, priority int, dueDate string)
+	AddTask(description string, priority int, dueDate time.Time)
 	Save()
 	LoadTasks(ch chan bool)
-	ViewTasks()
-	ViewCompletedTasks()
-	ViewIncompletedTasks()
+	ViewTasks() string
+	ViewCompletedTasks() string
+	ViewIncompletedTasks() string
 	TaskComplete(id int) string
 	DeleteTask(id int) string
 }
@@ -37,20 +39,20 @@ type TaskList struct {
 }
 
 type TaskRequest struct {
-	Command string
-	ID      int
+	Command     string
+	Description string
+	ID          int
+	Priority    int
+	DueDate     time.Time
 }
 
 var (
-	myList  Taskinator
-	command string
-	arg1    string
-	arg2    int
-	arg3    string
+	myList Taskinator
 
 	taskQueue = make(chan TaskRequest, 5)
 	results   = make(chan string)
 	wg        sync.WaitGroup
+	mu        sync.Mutex
 )
 
 func main() {
@@ -70,18 +72,15 @@ func main() {
 	*/
 	ch := make(chan bool)
 	ch2 := make(chan string)
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
-	close(taskQueue)
-	go Quit(quit)
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+	go Quit(signalChan)
 
-	status := "Loading Tasks..."
+	log.Println("Loading Tasks...")
 	go myList.LoadTasks(ch)
-	log.Println(status)
-	if <-ch == true {
-		status = "Done!"
+	if <-ch {
+		log.Println("Done!")
 	}
-	log.Println(status)
 
 	commands := map[string]string{
 		"Add new task":           "ADD-\"Task Description\"-Priority(0 to 3)-Due Date(YYYY//MM/DD)",
@@ -106,63 +105,77 @@ func main() {
 		fmt.Print("\nEnter a command: ")
 		input, _ := reader.ReadString('\n')
 
-		go func(input string) {
-			ParseInput(input)
+		// go func(input string) {
+		command, arg1, id, priority, dDate, err := ParseInput(input)
+		if err != nil {
+			fmt.Println("Error:", err)
+			// return
+			continue
+		}
+		// mu.Lock()
+		fmt.Println(<-ch2)
+		// mu.Unlock()
+		wg.Add(1)
+		taskQueue <- TaskRequest{Command: command, Description: arg1, ID: id, Priority: priority, DueDate: dDate}
+		fmt.Println(<-results)
 
-			switch command {
-			case "ADD":
-				myList.AddTask(arg1, arg2, arg3)
-			case "ALL":
-				myList.ViewTasks()
-			case "DONE":
-				myList.ViewCompletedTasks()
-			case "PENDING":
-				myList.ViewIncompletedTasks()
-			case "SAVE":
-				myList.Save()
-			default:
-				id, _ := strconv.Atoi(arg1)
-				go ProcessTask(command, id, ch2)
-				fmt.Println(<-ch2)
-			}
-		}(input)
+		// go ProcessTask(command, arg1, id, priority, dDate, ch2)
+		// fmt.Println(<-ch2)
+		// }(input)
 	}
 }
 
-func ParseInput(userInput string) {
+func ParseInput(userInput string) (command string, arg1 string, id int, priority int, dDate time.Time, err error) {
 	userInput = strings.TrimSpace(userInput)
 	inputSlice := strings.Split(userInput, "-")
 	command = strings.ToUpper(strings.TrimSpace(inputSlice[0]))
-	if len(inputSlice) <= 1 {
-		if command != "ADD" && command != "SAVE" && command != "DELETE" && command != "ALL" && command != "DONE" && command != "PENDING" && command != "MARK" {
+
+	if len(inputSlice) < 2 {
+		if slices.Contains([]string{"SAVE", "ALL", "DONE", "PENDING"}, command) {
+			return
 		}
-	} else if len(inputSlice) >= 2 {
-		arg1 = strings.TrimSpace(inputSlice[1])
-		arg2 = 3
-		arg3 = "2024/02/28"
-	}
-	if len(inputSlice) >= 3 {
-		arg2, _ = strconv.Atoi(strings.TrimSpace(inputSlice[2]))
-	}
-	if len(inputSlice) >= 4 {
-		arg3 = strings.TrimSpace(inputSlice[3])
+		return "", "", 0, 0, time.Time{}, fmt.Errorf("invalid command format")
 	}
 
+	switch command {
+	case "ADD":
+		if len(inputSlice) != 4 {
+			return "", "", 0, 0, time.Time{}, fmt.Errorf("ADD requires description, priority, and due date")
+		}
+		arg1 = strings.TrimSpace(inputSlice[1])
+		priority, err = strconv.Atoi(strings.TrimSpace(inputSlice[2]))
+		if err != nil {
+			return "", "", 0, 0, time.Time{}, fmt.Errorf("invalid priority value")
+		}
+		dDate, err = time.Parse("2006/01/02", strings.TrimSpace(inputSlice[3]))
+		if err != nil {
+			return "", "", 0, 0, time.Time{}, fmt.Errorf("invalid date format, use YYYY/MM/DD")
+		}
+	case "DELETE", "MARK":
+		id, err = strconv.Atoi(strings.TrimSpace(inputSlice[1]))
+		if err != nil {
+			return "", "", 0, 0, time.Time{}, fmt.Errorf("invalid ID")
+		}
+	default:
+		return "", "", 0, 0, time.Time{}, fmt.Errorf("unsupported command")
+	}
+	return
 }
 
-func ProcessTask(command string, id int, ch2 chan string) {
+func ProcessTask(command string, arg1 string, id int, priority int, dDate time.Time, ch2 chan string) {
 	// Add task to the wait group
 	// Send task ID to taskQueue
 	// Create a unique response channel per request
 	// Send/return the processed result to ch2
 
 	wg.Add(1)
-	taskQueue <- TaskRequest{Command: command, ID: id}
 	responseChan := make(chan string)
-	taskQueue <- TaskRequest{Command: command, ID: id}
-
+	taskQueue <- TaskRequest{Command: command, Description: arg1, ID: id, Priority: priority, DueDate: dDate}
+	// fmt.Printf("Command: %v\nDescription: %v\nID: %v\nPriority: %v\nDue Date: %v\n", command, arg1, id, priority, dDate)
 	go func() {
-		ch2 <- <-responseChan
+		result := <-responseChan
+		ch2 <- result
+		results <- result
 		close(responseChan)
 	}()
 }
@@ -170,9 +183,18 @@ func ProcessTask(command string, id int, ch2 chan string) {
 func (tList *TaskList) LoadTasks(ch chan bool) {
 	defer close(ch)
 
-	jsonData, _ := os.ReadFile("tasks.json")
-	json.Unmarshal(jsonData, &tList.tasks)
-	// fmt.Println("\nSaved tasks successfully loaded to memeory")
+	jsonData, err := os.ReadFile("tasks.json")
+	if err != nil {
+		log.Printf("Error reading tasks file: %v", err)
+		ch <- false
+		return
+	}
+	err = json.Unmarshal(jsonData, &tList.tasks)
+	if err != nil {
+		log.Printf("Error unmarshalling tasks: %v", err)
+		ch <- false
+		return
+	}
 	ch <- true
 }
 
@@ -182,7 +204,7 @@ func (tList *TaskList) Save() {
 	fmt.Println("File Updated!")
 }
 
-func (tList *TaskList) AddTask(description string, priority int, dueDate string) {
+func (tList *TaskList) AddTask(description string, priority int, dueDate time.Time) {
 	id := len(tList.tasks) + 1
 	for _, task := range tList.tasks {
 		if task.ID >= id {
@@ -196,30 +218,39 @@ func (tList *TaskList) AddTask(description string, priority int, dueDate string)
 		Priority:    priority,
 		DueDate:     dueDate,
 	}
-	tList.tasks = append(tList.tasks, task)
-	fmt.Println("Task added successfully")
-	myList.Save()
+	// fmt.Printf("Command: \nDescription: %v\nID: \nPriority: %v\nDue Date: %v\n", description, priority, dueDate)
+
+	if description != "" {
+		tList.tasks = append(tList.tasks, task)
+		fmt.Println("Task added successfully")
+		myList.Save()
+	} else {
+		fmt.Println("Empty description")
+	}
 }
 
-func (tList *TaskList) ViewTasks() {
-	fmt.Println("\nViewing all tasks")
-	fmt.Print("ID\t|Status\t|\tDescription\t| Priority | Due Date\n")
+func (tList *TaskList) ViewTasks() string {
+	var result strings.Builder
+	result.WriteString("\nViewing all tasks\n")
+	result.WriteString("ID\t|Status\t|\tDescription\t| Priority | Due Date\n")
 	if len(tList.tasks) > 0 {
 		for _, task := range tList.tasks {
 			status := "❌"
 			if task.Status {
 				status = "✅"
 			}
-			fmt.Printf("%v\t|%v\t|%v\t|%v\t|%v\n", task.ID, status, task.Description, task.Priority, task.DueDate)
+			result.WriteString(fmt.Sprintf("%v\t|%v\t|%v\t|%v\t|%v\n", task.ID, status, task.Description, task.Priority, task.DueDate))
 		}
 	} else {
-		fmt.Println("\tNo tasks for now")
+		result.WriteString("\tNo tasks for now\n")
 	}
+	return result.String()
 }
 
-func (tList *TaskList) ViewCompletedTasks() {
-	fmt.Println("\nViewing completed tasks")
-	fmt.Print("ID\t|Status\t|\tDescription\n")
+func (tList *TaskList) ViewCompletedTasks() string {
+	var result strings.Builder
+	result.WriteString("\nViewing completed tasks")
+	result.WriteString("ID\t|Status\t|\tDescription\n")
 	if len(tList.tasks) > 0 {
 		found := false
 		for _, task := range tList.tasks {
@@ -229,20 +260,23 @@ func (tList *TaskList) ViewCompletedTasks() {
 				if task.Status {
 					status = "✅"
 				}
-				fmt.Printf("%v\t|%v\t|%v\n", task.ID, status, task.Description)
+				result.WriteString(fmt.Sprintf("%v\t|%v\t|%v\n", task.ID, status, task.Description))
 			}
 		}
 		if !found {
-			fmt.Println("\tNo completed tasks yet")
+			result.WriteString("\tNo completed tasks yet")
 		}
 	} else {
-		fmt.Println("\tNo tasks for now")
+		result.WriteString("\tNo tasks for now")
 	}
+	return result.String()
+
 }
 
-func (tList *TaskList) ViewIncompletedTasks() {
-	fmt.Println("\nViewing incompleted tasks")
-	fmt.Print("ID\t|Status\t|\tDescription\n")
+func (tList *TaskList) ViewIncompletedTasks() string {
+	var result strings.Builder
+	result.WriteString("\nViewing incompleted tasks")
+	result.WriteString("ID\t|Status\t|\tDescription\n")
 	if len(tList.tasks) > 0 {
 		found := false
 		for _, task := range tList.tasks {
@@ -252,15 +286,16 @@ func (tList *TaskList) ViewIncompletedTasks() {
 				if task.Status {
 					status = "✅"
 				}
-				fmt.Printf("%v\t|%v\t|%v\n", task.ID, status, task.Description)
+				result.WriteString(fmt.Sprintf("%v\t|%v\t|%v\n", task.ID, status, task.Description))
 			}
 		}
 		if !found {
-			fmt.Println("\tNo completed tasks yet")
+			result.WriteString("\tNo completed tasks yet")
 		}
 	} else {
-		fmt.Println("\tNo tasks for now")
+		result.WriteString("\tNo tasks for now")
 	}
+	return result.String()
 }
 
 func (tList *TaskList) TaskComplete(id int) string {
@@ -294,7 +329,19 @@ func (tList *TaskList) DeleteTask(id int) string {
 func Worker(id int, tasks <-chan TaskRequest, results chan<- string) {
 	var result string
 	for task := range tasks {
+		// fmt.Printf("\tLOG FROM WORKER\nCommand: %v\nDescription: %v\nID: %v\nPriority: %v\nDue Date: %v\n", task.Command, task.Description, task.ID, task.Priority, task.DueDate)
 		switch task.Command {
+		case "ADD":
+			myList.AddTask(task.Description, task.Priority, task.DueDate)
+		case "ALL":
+			result = myList.ViewTasks()
+		case "DONE":
+			result = myList.ViewCompletedTasks()
+		case "PENDING":
+			result = myList.ViewIncompletedTasks()
+		case "SAVE":
+			myList.Save()
+			result = "Tasks saved successfully"
 		case "MARK":
 			result = myList.TaskComplete(task.ID)
 		case "DELETE":
@@ -311,7 +358,7 @@ func Quit(quit chan os.Signal) {
 	<-quit
 	fmt.Println("\nTidying things Up...")
 	myList.Save()
-	// close(taskQueue)
+	close(taskQueue)
 	wg.Wait()
 	fmt.Println("\nBis Bald!")
 	os.Exit(0)
